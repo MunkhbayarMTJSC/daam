50852b484f79671c95f3dcdd15e2e324b910970e
SiCA <munkhbayar.pch@gmail.com>
2025-06-04 20:52:01 +0800

    Deploy hiihed toxiruulav3

--- server/index.mjs
+++ server/index.mjs
@@ -3,7 +3,7 @@ import http from "http";
 import { Server } from "socket.io";
 import path from "path";
 import { fileURLToPath } from "url";
-import { GameRoom } from "./game/GameRoom.js";
+import { GameRoom } from "./rooms/GameRoom.js";
 
 // __dirname тохируулах
 const __filename = fileURLToPath(import.meta.url);
